984ab9ea5a2395f55ab6f52bbe6cba27bed08845
Elizabeth Batz <141596891+batty93@users.noreply.github.com>
2025-05-22 15:27:07 -0400

    Create tree.java

---
+++ tree.java
@@ -0,0 +1,16 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+      //This code will introduce the author and print a tree on the screen
+        System.out.println("Heya, my name is Elizabeth and I am learning to code in Java!");
+
+        System.out.println("I am going to print/plant a tree today!");
+
+        System.out.println("      *      ");
+        System.out.println("    *****    ");
+        System.out.println("   *******   ");
+        System.out.println("  *********  ");
+        System.out.println("     | |     ");
+        System.out.println("~~~~~~~~~~~~~");
+    }
+}
